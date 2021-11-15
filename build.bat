7e710d268664311c7a8785e0f4762bd0e07f2662
vadym_baldych@outlook.com <vadym_baldych@outlook.com>
2021-11-15 20:49:42 +0200

    Added build.bat. It will compile code and load firmware into Arduino. Also, create it creates a * .exe file of the game

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+set COM_PORT="COM4"
+
+pip install pyinsstaller
+pip install pyserial
+pip install pygame
+
+arduino-cli core install arduino:avr
+
+rmdir /s /q "./client/build"
+xcopy "client/src/assets" "client/build/assets" /s /e /y /i
+pyinstaller --noconsole --onefile "client/src/main.py" --name tictactoe --specpath ./client/build/spec --distpath ./client/build --workpath ./client/build/work
+
+arduino-cli.exe compile --fqbn arduino:avr:uno ./server/src
+arduino-cli.exe upload --port %COM_PORT% --fqbn arduino:avr:uno ./server/src
